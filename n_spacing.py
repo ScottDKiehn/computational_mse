f94c7e5889fcb2205a779cc836a8a2d3ba09cf4d
Scott Kiehn <scootyk1@gmail.com>
2023-10-10 14:08:49 -0700

    Update n_spacing.py

--- n_spacing.py
+++ n_spacing.py
@@ -9,11 +9,13 @@ def spacing(filename):
   with open("output.txt", 'w') as l:
     sys.stdout = l
 
+    
     with open(filename, "r") as f:
       data = f.read()
       xml_data = BeautifulSoup(data, "xml")
-      iteration_tags = xml_data.find_all("iteration")
+      iteration_tags = xml_data.find_all("iteration") #searches for "iteration" in xml file
 
+      #itialization
       iteration_count = 1
       n9_positions = []
       n10_positions = []
@@ -25,30 +27,32 @@ def spacing(filename):
         iteration_count += 1
 
         
-        atom_tags = iteration.find_all("atom")
-        for atom in atom_tags:
+        atom_tags = iteration.find_all("atom") #searches for "atom" in each iteration
+        for atom in atom_tags: #each atom has its respective data, gathers name and position data in the following lines:
           name = atom.get("name")
           atom_children = list(atom.children)
           position_data = atom_children[1].text
-          position_data_list_ang = [(float(val))/1.89 for val in position_data.split()]
+          position_data_list_ang = [(float(val))/1.89 for val in position_data.split()] #converts position to unit angrstoms
 
 
-          if name.startswith("N9"):
+          if name.startswith("N9"): #conditional to get postiions of nitrogen atom 9 and nitrogen atom 10
             n9_positions.append(position_data_list_ang)
           if name.startswith("N10"):
             n10_positions.append(position_data_list_ang)
 
-
+        #tried converting things to numpy arrays to make things easier
         n9_array, n10_array = np.array(n9_positions), np.array(n10_positions)
         n_difference = n9_array-n10_array
         n_differences.append(n_difference)
 
+
+        #lattice parameters (constant)
         lx = 5.45
         ly = 4.72
         lz = 12.14
 
-        n_difference_modified = [(n_difference[0] % lx), (n_difference[1] % ly), (n_difference[2] % lz)]
-        n_distance_true = [math.sqrt(sum(val**2 for val in n_difference_modified))]
+        n_difference_modified = [(n_difference[0] % lx), (n_difference[1] % ly), (n_difference[2] % lz)] #MODIFED: INCLUDES PERIODIC BOUNDARY CONDITIONS
+        n_distance_true = [math.sqrt(sum(val**2 for val in n_difference_modified))] #finds euclidean distance using pythagorean method
         n_differences_modified.append(n_difference_modified)
         n_distances_true.append(n_distance_true)
 
@@ -67,5 +71,5 @@ def spacing(filename):
       plt.show()
       plt.savefig('output.png', format='png', dpi=300, bbox_inches='tight', facecolor='white', edgecolor='black')
 
-spacing("ssages_out_0_run_0_cleaned_frame1.xml")
+spacing("ssages_out_0_run_0_cleaned_frame1.xml") #note this is only frame 1, normally there's many thousands
 
